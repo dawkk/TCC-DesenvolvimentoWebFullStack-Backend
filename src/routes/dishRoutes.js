5486e8db0190ae6929b770cabf4f3651001ad66f
dawkk <gventuri14@gmail.com>
2023-03-11 23:09:56 -0300

    Adding verify JWT and roles for dishes and menus

--- src/routes/dishRoutes.js
+++ src/routes/dishRoutes.js
@@ -1,19 +1,22 @@
 import express from "express";
 import DishController from "../controllers/dishesController.js";
+import verifyJWT from "../middlewares/verifyJWT.js";
+import verifyRoles from "../middlewares/verifyRoles.js";
+import ROLES_LIST from "../config/roles_list.js";
 
 const router = express.Router();
 
 /* quando projetar rotas precisamos colocar no topo da mais especifica para a menos especifica, ou teremos erros de código */
 
 router
-  .get("/dishes", DishController.listDishes)
-  .get("/dishes/search", DishController.listDishByType)
-  .get("/dishes/:id", DishController.listDishById)
-  .post("/dishes", DishController.createDish)
-  .put("/dishes/:id", DishController.updateDish)
-  .delete("/dishes/:id", DishController.deleteDish)
-  .post("/dishes/:id/image", DishController.uploadDishImage)
-  .get("/dishes/:id/image", DishController.getDishImage)
+  .get("/dishes",verifyJWT, verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor, ROLES_LIST.Employee), DishController.listDishes)
+  .get("/dishes/search",verifyJWT, verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor, ROLES_LIST.Employee), DishController.listDishByType)
+  .get("/dishes/:id",verifyJWT, verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor, ROLES_LIST.Employee), DishController.listDishById)
+  .post("/dishes",verifyJWT, verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor, ROLES_LIST.Employee), DishController.createDish)
+  .put("/dishes/:id",verifyJWT, verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor, ROLES_LIST.Employee), DishController.updateDish)
+  .delete("/dishes/:id",verifyJWT, verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), DishController.deleteDish)
+  .post("/dishes/:id/image",verifyJWT, verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor, ROLES_LIST.Employee), DishController.uploadDishImage)
+  .get("/dishes/:id/image",verifyJWT, verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor, ROLES_LIST.Employee), DishController.getDishImage)
 
 
 export default router;
--- src/routes/menuRoutes.js
+++ src/routes/menuRoutes.js
@@ -1,16 +1,19 @@
 import express from "express";
 import MenuController from "../controllers/menusController.js";
+import verifyJWT from "../middlewares/verifyJWT.js";
+import verifyRoles from "../middlewares/verifyRoles.js";
+import ROLES_LIST from "../config/roles_list.js";
 
 const router = express.Router();
 
 router.route("/menus")
-  .get(MenuController.listMenus)
-  .post(MenuController.createMenu)
+  .get(verifyJWT, verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor, ROLES_LIST.Employee), MenuController.listMenus)
+  .post(verifyJWT, verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor),MenuController.createMenu)
  
   router.route("/menus/:id")
-  .get(MenuController.listMenuById)
-  .put(MenuController.updateMenu)
-  .delete(MenuController.deleteMenu)
+  .get(verifyJWT, verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor, ROLES_LIST.Employee),MenuController.listMenuById)
+  .put(verifyJWT, verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor),MenuController.updateMenu)
+  .delete(verifyJWT, verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), MenuController.deleteMenu)
 
 
 
