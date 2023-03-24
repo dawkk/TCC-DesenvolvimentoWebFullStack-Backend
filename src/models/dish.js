d45273c0ab27d47ee73afc66ed83f772ceab7f10
dawkk <gventuri14@gmail.com>
2023-03-23 23:25:05 -0300

    Docker samples (users, menus, dishes) are now being loaded correctly upon compose

--- src/models/dish.js
+++ src/models/dish.js
@@ -1,15 +1,81 @@
 import mongoose from "mongoose";
 
 const dishSchema = new mongoose.Schema({
-  id: {type: String},
-  title: {type: String, required: true},
-  description: {type: String},
-  price: {type: Number},
-  menu: {type: mongoose.Schema.Types.ObjectId, ref:'menus', required: true},
-  type: {type: String},
-  image:{type: String}
+  id: { type: String },
+  title: { type: String, required: true },
+  description: { type: String },
+  price: { type: Number },
+  menu: { type: mongoose.Schema.Types.ObjectId, ref: 'menus', required: true },
+  type: { type: String },
+  image: { type: String }
 });
 
 const dishes = mongoose.model('dishes', dishSchema);
+
+dishes.countDocuments({}, (error, count) => {
+  if (error) {
+    console.error(error);
+  } else {
+    if (count === 0) {
+      const initialDishes = [
+        {
+          title: "Salada Simples",
+          description: "Alface americana, croutons crocantes, tomates-cereja frescos, parmesão ralado e molho ceasar.",
+          price: 25,
+          menu: "63e55b0716830cc85711c093",
+          type: "Entrada",
+          image: "image-1679521233614.jpg"
+        },
+        {
+          title: "Risoto de Frutos do Mar",
+          description: "Arroz arbóreo com camarão, lula, polvo e mariscos, temperados com azeite, alho, tomate e ervas.",
+          price: 58,
+          menu: "63e55b0216830cc85711c091",
+          type: "Prato Principal",
+          image: "image-1679521249097.jpg"
+        },
+        {
+          title: "Churrasco Misto",
+          description: "Seleção de carnes grelhadas, incluindo picanha, alcatra, fraldinha, linguiça e coração de frango, servida com farofa, vinagrete e pão de alho.",
+          price: 80,
+          menu: "63e55b0716830cc85711c093",
+          type: "Prato Principal",
+          image: "image-1679521263264.jpg"
+        },
+        {
+          title: "Yakissoba ",
+          description: "Macarrão frito com legumes, cogumelos, carne bovina e frango, em molho à base de shoyu e óleo de gergelim.",
+          price: 39.9,
+          menu: "63e55b1316830cc85711c095",
+          type: "Prato Principal",
+          image: "image-1679521279980.jpg"
+        },
+        {
+          title: "Ceviche de Salmão",
+          description: "Cubos de salmão marinados em suco de limão, temperados com coentro, rabanetes finos e pimenta dedo-de-moça e pimentão verde",
+          price: 42,
+          menu: "63e55b1916830cc85711c097",
+          type: "Entrada",
+          image: "image-1679521349769.jpg"
+        },
+        {
+          title: "Brigadeiro Gourmet",
+          description: "Delicioso doce de chocolate, feito com leite condensado, manteiga e cacau em pó, coberto com granulado.",
+          price: 5,
+          menu: "63e55b0716830cc85711c093",
+          type: "Sobremesa",
+          image: "image-1679521362929.jpg"
+        },
+      ];
+      dishes.insertMany(initialDishes, (error, docs) => {
+        if (error) {
+          console.error(error);
+        } else {
+          console.log(`${docs.length} dishes were successfully inserted into the database.`);
+        }
+      });
+    }
+  }
+});
 
 export default dishes;
--- src/models/user.js
+++ src/models/user.js
@@ -1,34 +1,118 @@
 import mongoose from "mongoose";
+import * as bcrypt from 'bcrypt';
 
 const userSchema = new mongoose.Schema({
-  firstName: {type: String, required: true},
-  lastName: {type: String},
-  email: {type: String, required: true},
-  password:{type: String, required: true},
-  cellphone:{type:String},
+  firstName: { type: String, required: true },
+  lastName: { type: String },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  cellphone: { type: String },
   /* address: {type: mongoose.Schema.Types.ObjectId, ref:'adresses', required: true}, */
   address: {
-    city: {type: String},
-    state: {type: String},
-    neighborhood: {type: String},
-    street: {type: String},
-    number: {type: Number},
-    zipcode: {type: Number}
+    city: { type: String },
+    state: { type: String },
+    neighborhood: { type: String },
+    street: { type: String },
+    number: { type: Number },
+    zipcode: { type: Number }
 
   },
   roles: {
     User: {
-      type:Number,
-      default:4000
+      type: Number,
+      default: 4000
     },
-    Employee: {type: Number},
-    Editor: {type: Number},
-    Admin: { type: Number}
+    Employee: { type: Number },
+    Editor: { type: Number },
+    Admin: { type: Number }
   },
-  refreshToken: {type: String},
-  status: {type: Boolean}
+  refreshToken: { type: String },
+  status: { type: Boolean }
 });
 
 const users = mongoose.model('users', userSchema);
+
+
+users.countDocuments({}, (error, count) => {
+  if (error) {
+    console.error(error);
+  } else {
+    if (count === 0) {
+      const initialUsers = [
+        {
+          firstName: "JohnUser",
+          lastName: "Doe",
+          email: "john@user.com",
+          password: "classified123",
+          cellphone: "(19)9999999999",
+          roles: {
+            User: 4000,
+          },
+        },
+        {
+          firstName: "JohnEmployee",
+          lastName: "Doe",
+          email: "john@employee.com",
+          password: "classified321",
+          cellphone: "(19)9999999999",
+          roles: {
+            User: 4000,
+            Employee: 3000,
+          },
+        },
+        {
+          firstName: "JohnEditor",
+          lastName: "Doe",
+          email: "john@editor.com",
+          password: "classified1556",
+          cellphone: "(19)9999999999",
+          roles: {
+            User: 4000,
+            Employee: 3000,
+            Editor: 2000,
+          },
+        },
+        {
+          firstName: "JohnAdmin",
+          lastName: "Doe",
+          email: "john@admin.com",
+          password: "classified1985",
+          cellphone: "(19)9999999999",
+          roles: {
+            User: 4000,
+            Employee: 3000,
+            Editor: 2000,
+            Admin: 1000,
+          },
+        },
+      ];
+
+      const saltRounds = 12;
+
+      initialUsers.forEach(async (user) => {
+        console.log('user', user, 'senha antes', user.password)
+        const passwordHash = await bcrypt.hash(user.password, saltRounds);
+        user.password = passwordHash;
+        console.log(user.password)
+
+        try {
+          users.insertMany(user, (error, docs) => {
+            if (error) {
+              console.error(error);
+            } else {
+              console.log(`${docs.length} users were successfully inserted into the database.`);
+            }
+          });
+        } catch (error) {
+          console.log(error)
+          return error
+        }
+      });
+    }
+  }
+});
+
+
+
 
 export default users;
