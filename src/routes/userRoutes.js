af2e65d78255a14da427475c9ddee9085b1d1a00
dawkk <gventuri14@gmail.com>
2023-03-30 11:32:05 -0300

    Updating test for users

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -15,9 +15,9 @@ router.route("/users")
   .post(UserController.createUser)
 
 router.route("/users/:id")
-  .get(verifyJWT, UserController.listUserById)
-  .put(verifyJWT, UserController.updateUser)
-  .delete(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), UserController.deleteUser)
+  .get(verifyJWT, verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor, ROLES_LIST.Employee), UserController.listUserById)
+  .put(verifyJWT, verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), UserController.updateUser)
+  .delete(verifyJWT, verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), UserController.deleteUser)
 
 /* router.route("/users/me")
 .get(verifyJWT, verifyRoles(ROLES_LIST.User), UserController.listSelf)
