df656e58ee93dda42685262da2222f7838889d80
dawkk <gventuri14@gmail.com>
2023-05-23 16:17:30 -0300

    Adding github oAuth

--- src/middlewares/oAuth.js
+++ src/middlewares/oAuth.js
@@ -3,8 +3,18 @@ import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import users from "../models/user.js";
-import dotenv from 'dotenv';
-dotenv.config();
+
+function generateRandomPassword() {
+  const length = 10;
+  const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charset.length);
+    password += charset[randomIndex];
+  }
+  return password;
+}
+
 
 passport.use(
   new GoogleStrategy(
@@ -57,16 +67,55 @@ passport.use(
   )
 );
 
-function generateRandomPassword() {
-  const length = 10;
-  const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-  let password = "";
-  for (let i = 0; i < length; i++) {
-    const randomIndex = Math.floor(Math.random() * charset.length);
-    password += charset[randomIndex];
-  }
-  return password;
-}
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: process.env.OAUTH_GITHUB_CLIENT_ID,
+      clientSecret: process.env.OAUTH_GITHUB_CLIENT_SECRET,
+      callbackURL: process.env.OAUTH_GITHUB_CALLBACK_URL,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        let user = await users.findOne({ email: profile.emails[0].value }).exec();
+
+        if (!user) {
+          const randomPassword = generateRandomPassword();
+
+          user = new users({
+            firstName: profile.displayName,
+            email: profile.emails[0].value,
+            password: randomPassword,
+            roles: {
+              User: 4000,
+            },
+            addresses: [],
+          });
+
+          const salt = await bcrypt.genSalt(12);
+          const passwordHash = await bcrypt.hash(randomPassword, salt);
+          user.password = passwordHash;
+
+          await user.save();
+        }
+        const tokenPayload = {
+          UserInfo: {
+            id: user._id,
+            roles: user.roles,
+            email: user.email,
+          },
+        };
+
+        const token = jwt.sign(tokenPayload, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '2h' });
+
+        done(null, token);
+      } catch (err) {
+        done(err);
+      }
+    }
+  )
+);
+
+
 
 
 export default passport;
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,17 +1,28 @@
 import express from "express";
 import passport from "../middlewares/oAuth.js";
-import dotenv from 'dotenv';
-dotenv.config();
+
 
 const router = express.Router();
 
 router.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 
 router.get('/auth/google/callback', passport.authenticate('google', { session: false }), (req, res) => {
-  const redirectUrl = process.env.FRONT_END_URL;
+  const redirectUrl = process.env.OAUTH_REDIRECT_URL;
   res.cookie('jwt', req.user, {
     httpOnly: true,
-    expires: new Date(Date.now() + 2 * 60 * 60 * 1000), // 2 hours
+    expires: new Date(Date.now() + 2 * 60 * 60 * 1000),
+  });
+  return res.redirect(redirectUrl);
+});
+
+
+router.get('/auth/github', passport.authenticate('github'));
+
+router.get('/auth/github/callback', passport.authenticate('github', { session: false }), (req, res) => {
+  const redirectUrl = process.env.OAUTH_REDIRECT_URL;
+  res.cookie('jwt', req.user, {
+    httpOnly: true,
+    expires: new Date(Date.now() + 2 * 60 * 60 * 1000),
   });
   return res.redirect(redirectUrl);
 });
